83b8a4a6665ac423f55aa1b2bb474f4da77b691c
Maria Fe Monares Sabar <mafs@marias-mbp.mynetworksettings.com>
2023-11-16 17:17:26 -0500

    2nd attempt

--- index.js
+++ index.js
@@ -3,66 +3,37 @@ const userInputString = prompt(
     "Please enter froyo flavor separated by commas.",
     "vanilla, strawberry, coffee, taro, banana"
   );
-  const froyoFlavor = {
-    vanilla: 3,
-    strawberry: 1,
-    Coffee: 2,
-  }; 
+  const stringArray = userInputString.split(",");
+
+
   console.log(Object.keys(froyoFlavor));
 
 
 
 // Split the string of numbers into an array of strings.
-const stringArray = userInputString.split(",");
+
 
 console.table(["vanilla", "straberry", "Coffee"])
 
-//let froyoFlavor = userInputString
-//console.log(Object.values(froyoFlavor));
 
 
-const froyoFlavor1 = {}
-  froyoFlavor1.name= 'vanilla';
-  froyoFlavor1.quantity= 3;
- 
-const froyoFlavor2 = {}
-  froyoFlavor2.name= 'strawberry';
-  froyoFlavor2.quantity= 1;
-
-const froyoFlavor3 = {}
-  froyoFlavor3.name= 'Coffee';
-  froyoFlavor3.quantity= 2;
-
-  // looping through an object
-for(const key in froyoFlavor1) {
-  console.log(key);
-  console.log(froyoFlavor1[key]);
-}
-
-for(const key in froyoFlavor2) {
-  console.log(key);
-  console.log(froyoFlavor2[key]);
-}
-
-for(const key in froyoFlavor3) {
-  console.log(key);
-  console.log(froyoFlavor3[key]);
-}
-
-function checkName(obj) {
+function flavorCount(array) {
   // let obj = froyoFlavor;
-  if(obj.name === "vanilla") {
-      obj.quantity = 3
-  } else if(obj.name === "strawberry") {
-      obj.quantity= 1}
-      else if(obj.name === "Coffee") {
-        obj.quantity = 2
-  } else {
-      obj.quantity = null;
+  const froyoFlavor = {
+    vanilla: 0,
+    strawberry: 0,
+    Coffee: 0,
+  }; 
+//for(initilization; condition; afterthought)
+for(i = 0; i < stringArray.length; i++){
+  const str = stringArray[i]
+  if (str === "vanilla") {
+      console.log(flavorCount.vanilla += 1);
+  } else (str === "strawberry") {
+    console.log(flavorCount.strawberry += 1);
+  } else (str === "Coffee") {
+    console.log(flavorCount.Coffee += 1);
   }
-
-  return obj;
+} return froyoFlavor;
   }
-  console.log(checkName(froyoFlavor1));
-  console.log(checkName(froyoFlavor2));
-  console.log(checkName(froyoFlavor3));
+console.table(flavorCount(stringArray))
